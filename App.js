a3581bbe4e61f4f05d9285fc34372ad92ddc1fb4
Sharad Shinde <shindesharad71@gmail.com>
2018-03-04 01:21:18 +0530

    App Navigation added

--- App.js
+++ App.js
@@ -1,68 +1,14 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { connect } from 'react-redux';
+import { Navigation } from 'react-native-navigation';
 
-import PlacesInput from './src/components/PlacesInput/PlacesInput';
-import PlacesList from './src/components/PlacesList/PlacesList';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index';
+import AuthScreen from './src/screens/Auth/Auth';
 
-class App extends React.Component {
+// Register Screens
+Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
 
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-  };
-
-  placeSelectedHandler = key => {
-    this.props.onSelectPlace(key);
-  };
-
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  };
-
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail 
-          selectedPlace={this.props.selectedPlace} 
-          onItemDeleted={this.placeDeletedHandler} 
-          onModalClosed={this.modalClosedHandler} />
-        <PlacesInput onPlaceAdded={this.placeAddedHandler}  />
-        <PlacesList places={this.props.places} onItemSelected={this.placeSelectedHandler} />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 26,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+// Start App
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesome-places.AuthScreen",
+    title: "Login"
   }
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: name => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onSelectPlace: key => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- android/app/src/main/java/com/nativeapp/MainActivity.java
+++ android/app/src/main/java/com/nativeapp/MainActivity.java
@@ -1,15 +1,8 @@
 package com.nativeapp;
 
+import com.reactnativenavigation.controllers.SplashActivity;
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "NativeApp";
-    }
 }
