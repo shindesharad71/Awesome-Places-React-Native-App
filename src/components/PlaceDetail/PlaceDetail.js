42327525c97e99a79091b3b6dc187a621237f3c0
Sharad Shinde <shindesharad71@gmail.com>
2018-03-03 20:24:40 +0530

    icons added and used, packager bug fixed

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Modal, View, Image, Text, Button, StyleSheet } from 'react-native';
+import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const PlaceDetail = props => {
     let modalContent = null;
@@ -18,7 +20,11 @@ const PlaceDetail = props => {
             <View style={styles.modalContainer}>
                 {modalContent}
                 <View>
-                    <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+                    <TouchableOpacity onPress={props.onItemDeleted}>
+                        <View style={styles.deleteButton}>
+                            <Icon size={30} name="ios-trash" color="red" />
+                        </View>
+                    </TouchableOpacity>
                     <Button title="Close" onPress={props.onModalClosed} />
                 </View>
             </View>
@@ -38,6 +44,9 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         textAlign: "center",
         fontSize: 28
+    },
+    deleteButton: {
+        alignItems: "center"
     }
 });
 
