4c71626b15f121d033de92914f33230ee3beac67
Sharad Shinde <shindesharad71@gmail.com>
2018-03-04 16:05:32 +0530

    Demo updated

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -6,6 +6,21 @@ import PlaceList from '../../components/PlacesList/PlacesList';
 
 class FindPlaceScreen extends Component {
 
+    constructor(props) {
+        super(props);
+        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+    }
+
+    onNavigatorEvent = event => {
+        if(event.type === "NavBarButtonPress") {
+            if(event.id === "sideDrawerToggle") {
+                this.props.navigator.toggleDrawer({
+                    side: "left"
+                })
+            }
+        }
+    }
+
     itemSelectedHandler = key => {
         const selPlace = this.props.places.find(place => {
             return place.key === key;
--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -4,11 +4,19 @@ import { View, Text, Dimensions, StyleSheet } from 'react-native';
 class SideDrawer extends Component {
     render() {
         return (
-            <View style={{width: Dimensions.get("window").width * 0.8}}>
+            <View style={[styles.container, {width: Dimensions.get("window").width * 0.8}]}>
                 <Text>SideDrawer</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 22,
+        backgroundColor: "white",
+        flex: 1
+    }
+});
 
 export default SideDrawer;
