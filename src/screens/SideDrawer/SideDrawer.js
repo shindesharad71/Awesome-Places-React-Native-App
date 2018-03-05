835f2e27cfc6f2e2c0a548e88e9017c1d9ee9c38
Sharad Shinde <shindesharad71@gmail.com>
2018-03-05 22:56:30 +0530

    Platform OS according icons addded

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 class SideDrawer extends Component {
@@ -8,7 +8,7 @@ class SideDrawer extends Component {
             <View style={[styles.container, {width: Dimensions.get("window").width * 0.8}]}>
                 <TouchableOpacity>
                     <View style={styles.drawerItem}>
-                        <Icon name="ios-log-out" size={30} color="#aaa" style={styles.drawerItemIcon} />
+                        <Icon name={Platform.OS === 'android' ? "md-log-out" : "ios-log-out"} size={30} color="#aaa" style={styles.drawerItemIcon} />
                         <Text>Sign Out</Text>
                     </View>
                 </TouchableOpacity>  
