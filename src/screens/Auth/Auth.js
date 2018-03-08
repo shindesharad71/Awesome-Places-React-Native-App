18c52c07eb0eaa45c0aff99f2786445b3e948a55
Sharad Shinde <shindesharad71@gmail.com>
2018-03-08 23:35:55 +0530

    soft keyboard layout

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -148,6 +148,9 @@ class AuthScreen extends Component {
               onChangeText={(val) => this.updateInputState('email', val)}
               valid={this.state.controls.email.valid}
               touched={this.state.controls.email.touched}
+              autoCapitalize={false}
+              autoCorrect={false}
+              keyboardType="email-address"
             />
             <View
               style={
@@ -170,6 +173,7 @@ class AuthScreen extends Component {
                   onChangeText={(val) => this.updateInputState('password', val)}
                   valid={this.state.controls.password.valid}
                   touched={this.state.controls.password.touched}
+                  secureTextEntry
                  />
               </View>
               <View
@@ -186,6 +190,7 @@ class AuthScreen extends Component {
                   onChangeText={(val) => this.updateInputState('confirmPassword', val)}
                   valid={this.state.controls.confirmPassword.valid}
                   touched={this.state.controls.confirmPassword.touched}
+                  secureTextEntry
                 />
               </View>
             </View>
