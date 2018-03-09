6cc31866662989dfa05e0bc51631e0fb47d8a581
Sharad Shinde <shindesharad71@gmail.com>
2018-03-09 23:16:12 +0530

    React native maps added

--- android/app/src/main/java/com/nativeapp/MainApplication.java
+++ android/app/src/main/java/com/nativeapp/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -57,7 +58,8 @@ public class MainApplication extends NavigationApplication {
          // No need to add RnnPackage and MainReactPackage
          return Arrays.<ReactPackage>asList(
              // eg. new VectorIconsPackage()
-             new VectorIconsPackage()
+             new VectorIconsPackage(),
+             new MapsPackage()
          );
      }
 
