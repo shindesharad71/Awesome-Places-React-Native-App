a3a0a3fe5ce80960ecef4840adc177cf1384a660
Sharad Shinde <shindesharad71@gmail.com>
2018-03-10 23:58:15 +0530

    Image Picker inserted into pickimage page

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
 import { View, Image, Button, StyleSheet } from "react-native";
-
-import imagePlaceholder from "../../assets/beautiful-place.jpg";
+import ImagePicker from 'react-native-image-picker';
 
 class PickImage extends Component {
+
+  state = {
+    pickedImage: null
+  }
+
+  pickImageHandler = () => {
+    ImagePicker.showImagePicker({title: "Pick an Image"}, res => {
+      if(res.didCancel) {
+        console.log('User Canceled');
+      } else if(res.error) {
+        console.log('Error', res.error);
+      } else {
+        this.setState({
+          pickedImage: {
+            uri: res.uri
+          }
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.placeholder}>
-          <Image source={imagePlaceholder} style={styles.previewImage} />
+          <Image source={this.state.pickedImage} style={styles.previewImage} />
         </View>
         <View style={styles.button}>
-          <Button title="Pick Image" onPress={() => alert('Pick Image!')} />
+          <Button title="Pick Image" onPress={this.pickImageHandler} />
         </View>
       </View>
     );
