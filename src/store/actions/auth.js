370dffb46b84dedf2f001b05ea9894c4b2712ca8
Sharad Shinde <shindesharad71@gmail.com>
2018-03-11 19:16:33 +0530

    Auth screen login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,40 @@
 import { TRY_AUTH } from './actionTypes';
+import { uiStartLoading, uiStopLoading } from './index';
+import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
 export const tryAuth = (authData) => {
-    return {
-        type: TRY_AUTH,
-        authData: authData
+    return dispatch => {
+        dispatch(authSignup(authData));
+    };
+};
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        dispatch(uiStartLoading());
+        fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAfGWjbMqED-ls4gUzfKRD0GzzXDDk9RWE", {
+            method: "POST",
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            alert("Authentication failed, please try again!");
+            dispatch(uiStopLoading());
+        })
+        .then(res => res.json())
+        .then(parsedRes => {
+            dispatch(uiStopLoading());
+            if(parsedRes.error) {
+                alert("Authentication failed, please try again!");
+            } else {
+                startMainTabs();
+            }
+        });
     };
 };
