3a4afcb229304bac1d02d212875ee5499c129b94
Sharad Shinde <shindesharad71@gmail.com>
2018-03-11 13:45:58 +0530

    POST data to firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,19 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: ADD_PLACE,
-        placeName: placeName,
-        location: location,
-        image: image
+    return dispatch => {
+        const placeData = {
+            placeName: placeName,
+            location: location
+        };
+        fetch("https://awesome-places-r-1520617147968.firebaseio.com/places.json", {
+            method: "POST",
+            body: JSON.stringify(placeData)
+        }).catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
     };
 };
 
