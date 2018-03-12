6c5e03f29084f8f54bba25eaad645f8b36742f67
Sharad Shinde <shindesharad71@gmail.com>
2018-03-12 22:06:38 +0530

    Auth token is stored in redux

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -2,3 +2,4 @@ export const SET_PLACES = 'SET_PLACES';
 export const TRY_AUTH = 'TRY_AUTH';
 export const UI_START_LOADING = 'UI_START_LOADING';
 export const UI_STOP_LOADING = 'UI_STOP_LOADING';
+export const AUTH_SET_TOKEN = 'AUTH_SET_TOKEN';
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,10 +1,10 @@
-import { TRY_AUTH } from './actionTypes';
+import { TRY_AUTH, AUTH_SET_TOKEN } from './actionTypes';
 import { uiStartLoading, uiStopLoading } from './index';
 import startMainTabs from '../../screens/MainTabs/startMainTabs';
 
 export const tryAuth = (authData, authMode) => {
     return dispatch => {
-        //dispatch(uiStartLoading());
+        dispatch(uiStartLoading());
         const apiKey = 'AIzaSyAfGWjbMqED-ls4gUzfKRD0GzzXDDk9RWE';
         let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key='+apiKey;
         if(authMode === 'signup') {
@@ -30,11 +30,19 @@ export const tryAuth = (authData, authMode) => {
         .then(res => res.json())
         .then(parsedRes => {
             dispatch(uiStopLoading());
-            if(parsedRes.error) {
+            if(!parsedRes.idToken) {
                 alert("Authentication failed, please try again!");
             } else {
+                dispatch(authSetToken(parsedRes.idToken));
                 startMainTabs();
             }
         });
+    };
+};
+
+export const authSetToken = token => {
+    return {
+        type: AUTH_SET_TOKEN,
+        token: token
     };
 };
