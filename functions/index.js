526c2f477439f76bc08dc4a86c31223f9e41056a
Sharad Shinde <shindesharad71@gmail.com>
2018-03-12 22:56:42 +0530

    Firebase function is updated

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 const fs = require('fs');
 const UUID = require('uuid-v4');
@@ -10,12 +11,25 @@ const gcconfig = {
 
 const gcs = require('@google-cloud/storage')(gcconfig);
 
+admin.initializeApp({
+    credential: admin.credential.cert(require('./awesome-places.json'))
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.storeImage = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
-        const body = JSON.parse(request.body);
+        if(!request.header.authorization || !request.header.authorization.startsWith("Bearer ")) {
+            console.log('No token present!');
+            response.status(403).json({error: "Unauthorized"});
+            return;
+        }
+        let idToken;
+        idToken = request.headers.authorization.split("Bearer ")[1];
+        admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            const body = JSON.parse(request.body);
         fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', err => {
             console.log(err);
             return response.status(500).json({error: err});
@@ -43,9 +57,13 @@ exports.storeImage = functions.https.onRequest((request, response) => {
                 uuid
             });
         } else {
-            console.log(err);
+            console.log('Invalid Token');
             response.status(500).json({error: err});
         }
     });
+        }).catch(error => {
+            conole.log('Token is invalid');
+            response.status(403).json({error: "Unauthorized"});
+        });
     });
 });
